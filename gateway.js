2897246b61952f0a935e7c9055b1694f681dc7cc
codebyazeredo <matheus.azeredo@hotmail.com>
2025-07-15 15:38:13 -0300

    feat: config de logs estruturados

--- gateway.js
+++ gateway.js
@@ -1,3 +1,29 @@
 require('dotenv').config();
 
+const express = require('express');
+const oracledb = require('oracledb');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const winston = require('winston');
+const jwt = require('jsonwebtoken');
+
+const { check, validationResult } = require('express-validator');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+
+
 
